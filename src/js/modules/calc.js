c6ca9b3d8d936914558ee7ecc179762bf500acb5
Dmytro Shaposhnikov <dimacr137@gmail.com>
2024-10-26 17:03:59 +0300

    osp upd

--- src/js/modules/calc.js
+++ src/js/modules/calc.js
@@ -1,11 +1,55 @@
+import { getResource } from "../services/requests";
+
 const calc = (size, material, options, promocode, result) => {
     const sizeBlock = document.querySelector(size),
           materialBlock = document.querySelector(material),
           optionsBlock = document.querySelector(options),
           promocodeBlock = document.querySelector(promocode),
-          resultBlock = document.querySelector(result);
+          resultBlock = document.querySelector(result),
+          sizeOptions = document.querySelectorAll('#size option'),
+          materialOptions = document.querySelectorAll('#material option'),
+          optionOptions = document.querySelectorAll('#options option'),
 
-    let sum = 0;
+          sum = 0;
+
+    const currentPrices = async (api, selector) => {
+        return getResource(api)
+            .then(item => {
+                setOption(item, sizeOptions);
+                setOption(item, materialOptions);
+                setOption(item, optionOptions);
+            })
+    }
+
+    const setOption = (arr, selector) => {
+        let obj = arr[0];
+        let keys = Object.keys(obj);
+        keys.pop()
+
+        const values = [];
+
+
+        selector.forEach((item, i) => {
+            if (i < keys.length) {
+                item.value = obj[keys[i]];
+                values.push(item.value)
+
+            }
+        })
+
+        console.log(values)
+
+    }
+
+
+
+
+
+     currentPrices('http://localhost:3000/size', sizeOptions);
+     currentPrices('http://localhost:3000/material', materialOptions);
+    currentPrices('http://localhost:3000/options', optionOptions);
+
+
 
     const calcFunc = () => {
         sum = Math.round((+sizeBlock.value) * (+materialBlock.value) + (+optionsBlock.value));
@@ -19,9 +63,9 @@ const calc = (size, material, options, promocode, result) => {
         }
     };
 
-    sizeBlock.addEventListener('change', calcFunc);
-    materialBlock.addEventListener('change', calcFunc);
-    optionsBlock.addEventListener('change', calcFunc);
+    sizeBlock.addEventListener('change', calcFunc, currentPrices);
+    materialBlock.addEventListener('change', calcFunc, currentPrices);
+    optionsBlock.addEventListener('change', calcFunc, currentPrices);
     promocodeBlock.addEventListener('input', calcFunc);
 
 };
